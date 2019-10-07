cc21224098c078ccf57712b40d2b3a339ccecfd5
Juncheng LIU <juncheng.liu@outlook.com>
2019-10-07 14:38:28 +0200

    add templates and urls configurations

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.http import HttpResponse
+ 
+def index(request):
+    return render(request, 'blog/index.html', context={
+        'title': "JCC's Blog",
+        'welcome': 'Welcome to my Blog'
+    })
